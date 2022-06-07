30c89ce62a608f44f44f324291013be81589a46a
phosfox <mordredur@googlemail.com>
2022-06-07 18:45:20 +0200

    init commit

---
+++ web/src/pages/AboutPage/AboutPage.tsx
@@ -0,0 +1,12 @@
+import { MetaTags } from '@redwoodjs/web'
+
+const AboutPage = () => {
+  return (
+    <>
+      <MetaTags title="About" description="About page" />
+      <p>This is just a sentence.</p>
+    </>
+  )
+}
+
+export default AboutPage
